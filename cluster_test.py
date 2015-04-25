fe9795129f240ad1901763081b6d748de65997fd
James Davenport <jradavenport@gmail.com>
2015-04-24 17:53:42 -0700

    tweak with plot

--- cluster_test.py
+++ cluster_test.py
@@ -152,7 +152,12 @@ Y_ = clf.predict(X_train)
 
 #-- follow DBSCAN example from:
 # http://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html#example-cluster-plot-dbscan-py
-db = DBSCAN(eps=10,min_samples=3, algorithm='kd_tree').fit(X2d)
+
+#Xdbs = X2d # this now works in 2D
+Xdbs = X_train # now do it in the 3D space
+
+db = DBSCAN(eps=10, min_samples=2, algorithm='kd_tree').fit(Xdbs)
+
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 core_samples_mask[db.core_sample_indices_] = True
 labels = db.labels_
@@ -161,7 +166,8 @@ n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 
 # Black removed and is used for noise instead.
 unique_labels = set(labels)
-colors = cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+colors = cm.Paired( np.linspace(0, 1, len(unique_labels)) )
+
 for k, col in zip(unique_labels, colors):
     if k == -1:
         # Black used for noise.
@@ -169,14 +175,18 @@ for k, col in zip(unique_labels, colors):
 
     class_member_mask = (labels == k)
 
-    xy = X2d[class_member_mask & core_samples_mask]
-    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=14)
+    xy = Xdbs[class_member_mask & core_samples_mask]
+    plt.scatter(xy[:, 0], xy[:, 1], c=col,
+            s=(xy[:, 2] / np.nanmax(r1)*20.)**2. )
 
-    xy = X2d[class_member_mask & ~core_samples_mask]
-    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=6)
+    xy = Xdbs[class_member_mask & ~core_samples_mask]
+    plt.scatter(xy[:, 0], xy[:, 1], c=col,
+             alpha=0.25, s=(xy[:, 2] / np.nanmax(r1)*20.)**2.)
 
-plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.title('DBSCAN: Estimated number of clusters: %d' % n_clusters_)
+plt.xlabel('Time (BJD - 2454833 days)')
+plt.ylabel('Longitude (deg)')
+plt.xlim((np.min(tmid), np.max(tmid)))
+plt.ylim((0,360))
 plt.show()
 
