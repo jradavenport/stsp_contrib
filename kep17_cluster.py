3ddce84fd4196c4b99fdf62d802f520e51a4b4b6
James Davenport <jradavenport@gmail.com>
2015-04-30 15:45:01 -0700

    first run of clustering works

--- kep17_cluster.py
+++ kep17_cluster.py
@@ -16,13 +16,12 @@ from sklearn import metrics
 mpl.rcParams['font.size'] = 16
 
 
-
 workingdir = '/astro/store/scratch/jrad/stsp/n8s/'
 actionL = True # has the Action=L rerun been done to make vis files?
 bump_lim = 1 # number of epochs bump must exist for
 
 
-per = 10.0 # the rotation period used to fold this data and fed to STSP previous to this
+per = 12.25817669188 # the rotation period used to fold this data and fed to STSP previous to this
 
 # read in each parambest file, save as big structure
 os.system('ls ' + workingdir + '*parambest*.txt > parambest.lis')
@@ -49,7 +48,7 @@ for n in range(len(pbestfile)):
 
     # read in the lcbest file (or lcout)
     if actionL is True:
-        tn,fn,en,mn,flg,x = np.loadtxt(pbestfile[n].replace('parambest', 'L_lcout'),
+        tn,fn,en,mn,flg = np.loadtxt(pbestfile[n].replace('parambest', 'L_lcout'),
                                      dtype='float', unpack=True)
     else:
         tn,fn,en,mn = np.loadtxt(pbestfile[n].replace('parambest', 'lcbest'),
@@ -60,9 +59,9 @@ for n in range(len(pbestfile)):
     tmid[n] = np.median(tn)
 
     for i in range(int(nspt)):
-        r1[n,i] = t[np_l + 1 + i*3.]
-        x1[n,i] = t[np_l + 2 + i*3.]
-        y1[n,i] = t[np_l + 3 + i*3.]
+        r1[n,i] = t[np_l + 1 + i*3.] # radius
+        x1[n,i] = t[np_l + 2 + i*3.] # lat
+        y1[n,i] = t[np_l + 3 + i*3.] # lon
 
         k = np.mod(flg, 2)
         in_trans[n,i] = (k == 1.).sum()
@@ -71,89 +70,49 @@ for n in range(len(pbestfile)):
 
 tmid_nspt = np.repeat(tmid, nspt).reshape((len(tmid), nspt))
 
-tlim = 1400.0
+tlim = 1600.0
 yes1 = np.where((in_trans.ravel() >= bump_lim) & (tmid_nspt.ravel() < tlim))
 
-xo = tmid_nspt.ravel()[yes1]
-yo = y1.ravel()[yes1]
-zo = r1.ravel()[yes1]
-data3d = np.squeeze(np.array([ [xo], [yo], [zo] ]))
-X_train = data3d.T
+xo = tmid_nspt.ravel()[yes1] # time
+yo = y1.ravel()[yes1] # lon
+zo = r1.ravel()[yes1] # rad
+lo = x1.ravel()[yes1] # lat
 
 data2d = np.squeeze(np.array([ [xo], [yo] ]))
 X2d = data2d.T
+
+data3d = np.squeeze(np.array([ [xo], [yo], [zo] ]))
+X3d = data3d.T
+
+data4d = np.squeeze(np.array([ [xo], [yo], [zo], [lo] ]))
+X4d = data4d.T
+
 
 # the data is now ready for clustering!!
 ###############################################
 
 # The general plot, replicate from IDL work
-# plt.figure()
-# for k in range(int(nspt)):
-#     yes = np.where((in_trans[:,k] >= bump_lim))
-#     plt.scatter(tmid[yes], y1[yes,k], cmap=cm.gnuplot2_r, c=(r1[yes,k]), alpha=0.6,
-#                 s=(r1[yes,k] / np.nanmax(r1)*20.)**2.)
-# plt.xlim((np.min(tmid), np.max(tmid)))
-# plt.ylim((0,360))
-# plt.xlabel('Time (BJD - 2454833 days)')
-# plt.ylabel('Longitude (deg)')
-# plt.title('In-Transit Spots Only')
-# cb = plt.colorbar()
-# cb.set_label('spot radius')
-# plt.show()
+plt.figure()
+for k in range(int(nspt)):
+    yes = np.where((in_trans[:,k] >= bump_lim))
+    plt.scatter(tmid[yes], y1[yes,k], cmap=cm.gnuplot2_r, c=(r1[yes,k]), alpha=0.6,
+                s=(r1[yes,k] / np.nanmax(r1)*20.)**2.)
+plt.xlim((np.min(tmid), np.max(tmid)))
+plt.ylim((0,360))
+plt.xlabel('Time (BJD - 2454833 days)')
+plt.ylabel('Longitude (deg)')
+plt.title('In-Transit Spots Only')
+cb = plt.colorbar()
+cb.set_label('spot radius')
+plt.show()
 #####################
-
-# now try different clustering approaches
-
-
-#-- GMM
-np.random.seed(0)
-clf = mixture.GMM(n_components=32, covariance_type='full')
-clf.fit(X_train)
-Y_ = clf.predict(X_train)
-
-# make plot of all the ellipses
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# color_iter = itertools.cycle(['r', 'g', 'b', 'c', 'm' ,'k','y'])
-# angle_out = np.zeros(clf.n_components)
-# for i in range(clf.n_components):
-#     means = clf.means_[i, 0:2]
-#     covar = clf._get_covars()[i][0:2, 0:2]
-#
-#     v, w, = linalg.eigh(covar)
-#     angle = np.arctan2(w[0][1], w[0][0]) * 180.0 / np.pi
-#     angle_out[i] = angle
-#
-#     ell = Ellipse(means, v[0], v[1], angle)
-#     ax.add_artist(ell)
-#     ell.set_clip_box(ax.bbox)
-#     ell.set_facecolor('None')
-#     ell.set_fill(False)
-#     ell.set_color(color_iter.next())
-#
-# plt.xlim((np.min(tmid), np.max(tmid)))
-# plt.ylim((0,360))
-# plt.scatter(xo, yo, c='k', s=(zo / np.nanmax(r1)*20.)**2., alpha=0.6)
-# plt.show()
-
-# make standard plot, color by cluster
-# plt.figure()
-# plt.scatter(xo, yo, c=Y_, s=(zo / np.nanmax(r1)*20.)**2., cmap=cm.Paired, alpha=0.6)
-# plt.xlabel('Time (BJD - 2454833 days)')
-# plt.ylabel('Longitude (deg)')
-# plt.title('Example GMM')
-# plt.xlim((np.min(tmid), np.max(tmid)))
-# plt.ylim((0,360))
-# cb = plt.colorbar()
-# cb.set_label('GMM component #')
-# plt.show()
 
 
 #-- follow DBSCAN example from:
 # http://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html#example-cluster-plot-dbscan-py
 
 #Xdbs = X2d # this now works in 2D
-Xdbs = X_train # now do it in the 3D space
+Xdbs = X3d # now do it in the 3D space
 
 db = DBSCAN(eps=10, min_samples=2, algorithm='kd_tree').fit(Xdbs)
 
