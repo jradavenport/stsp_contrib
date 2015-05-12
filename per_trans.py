6935b753b073af3731edc8f8fa3927622502a859
James Davenport <jradavenport@gmail.com>
2015-05-12 08:53:50 -0700

    got it

--- per_trans.py
+++ per_trans.py
@@ -39,10 +39,10 @@ t_0 = str(params[1] - BJDREF)
 tdur = str(params[2])
 p_rot = str(params[3])
 Rp_Rs = str(params[4]**2.)
-impact = str(params[5])
-incl_orb = str(params[6])
-Teff = str(params[7])
-sden = str(params[8])
+# impact = str(params[5])
+# incl_orb = str(params[6])
+# Teff = str(params[7])
+# sden = str(params[8])
 
 
 # figure out number of spots by reading in first parambest file
@@ -72,21 +72,42 @@ for n in range(len(pbestfile)):
     t = np.loadtxt(pbestfile[n], dtype='float', usecols=(0,), unpack=True, delimiter=' ')
 
     # step thru every transit in this lcbest file
-    trans0 = np.round((tn[0] - float(t_0))/float(p_orb))
-    trans1 = np.round((tn[-1] - float(t_0))/float(p_orb))
+    trans0 = int(np.round((tn[0] - float(t_0))/float(p_orb)))
+    trans1 = int(np.round((tn[-1] - float(t_0))/float(p_orb)))
     for j in range(trans0, trans1):
         trans_j = float(t_0) + float(p_orb)*j
         intrans = np.where((tn >= trans_j - float(tdur)*0.5) &
                            (tn <= trans_j + float(tdur)*0.5))
 
-        # for each transit, compute local chisq
-        chisq_tr = np.append(chisq_tr, np.sum( ((mn[intrans] - fn[intrans]) / en[intrans])**2.0 ))
+        if len(intrans[0])>0:
+            # for each transit, compute local chisq
+            chisq_tr = np.append(chisq_tr, np.sum( ((mn[intrans] - fn[intrans]) / en[intrans])**2.0 ))
 
-        tmid_tr = np.append(tmid_tr, np.mean(tn[intrans]))
+            tmid_tr = np.append(tmid_tr, np.mean(tn[intrans]))
 
-        rad = np.vstack( (rad, t[[np_l + 1 + i*3.]]) )
-        lat = np.vstack( (lat, t[[np_l + 2 + i*3.]]) )
-        lon = np.vstack( (lon, t[[np_l + 3 + i*3.]]) )
+            flg_tr = flg[intrans]
+            r1 = np.zeros(nspt)
+            x1 = np.zeros(nspt)
+            y1 = np.zeros(nspt)
+            in_trans = np.zeros(nspt)
+            for i in range(int(nspt)):
+                r1[i] = t[np_l + 1 + i*3.] # radius
+                x1[i] = t[np_l + 2 + i*3.] # lat
+                y1[i] = t[np_l + 3 + i*3.] # lon
+
+                k = np.mod(flg_tr, 2)
+                in_trans[i] = (k == 1.).sum()
+                flg_tr = (flg_tr - k)/2.0
+
+            bad = np.where((in_trans<1)) # find spots not in THIS transit
+            if len(bad[0])>0:
+                r1[bad] = -99 # give them bad values
+                x1[bad] = -99
+                y1[bad] = -99
+
+            rad = np.vstack( (rad, r1) )
+            lat = np.vstack( (lat, x1) )
+            lon = np.vstack( (lon, y1) )
 
 rad = rad[1:,:] # get rid of first empty row
 lat = lat[1:,:]
@@ -109,6 +130,25 @@ for j in range(int( (max(tmid_tr)-min(tmid_tr))/float(p_orb)+1 )):
         best = np.argmin(chisq_tr[xtr])
 
         tmid_out = np.append(tmid_out, trans_j)
-        rad_out = np.vstack( (rad_out, rad[xtr,:][best]) )
-        lat_out = np.vstack( (lat_out, lat[xtr,:][best]) )
-        lon_out = np.vstack( (lon_out, lon[xtr,:][best]) )
+        rad_out = np.vstack( (rad_out, rad[xtr,:][:,best]) )
+        lat_out = np.vstack( (lat_out, lat[xtr,:][:,best]) )
+        lon_out = np.vstack( (lon_out, lon[xtr,:][:,best]) )
+
+rad_out = rad_out[1:,:] # get rid of first empty row
+lat_out = lat_out[1:,:]
+lon_out = lon_out[1:,:]
+
+plt.figure(figsize=(12,6))
+for k in range(int(nspt)):
+    yes = np.where((rad_out[:,k] > 0)) # pick spots not = -99
+    plt.scatter(tmid_out[yes], lon_out[yes,k], alpha=0.6,
+                cmap=cm.gnuplot2_r, c=(rad_out[yes,k]),
+                s=(rad_out[yes,k] / np.nanmax(rad_out)*20.)**2.)
+plt.xlim((np.min(tmid_out), np.max(tmid_out)))
+plt.ylim((0,360))
+plt.xlabel('Time (BJD - 2454833 days)')
+plt.ylabel('Longitude (deg)')
+plt.title('Best Transit Spots')
+cb = plt.colorbar()
+cb.set_label('spot radius')
+plt.show()
