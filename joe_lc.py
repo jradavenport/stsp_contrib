32578f1bca17d2eb6eb0ad6a569a022f324dca84
James Davenport <jradavenport@gmail.com>
2015-05-13 09:05:06 -0700

    make new figure

---
+++ joe_lc.py
@@ -0,0 +1,96 @@
+'''
+Make figure(s) introducing Joe Llama's model light curve he has provide us
+
+can be run on the laptop, just run in the dropbox dir
+'''
+
+
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+
+# dropboxdir = '/astro/users/jrad/Dropbox/python/kepler17/llama_model/'
+dropboxdir = '/Users/james/Dropbox/python/kepler17/llama_model/'
+
+prefix = 'joe'
+BJDREF = 0.
+
+# the parameter file detailing the system
+pfile = 'llama.params'
+
+params = np.loadtxt(dropboxdir + pfile,unpack=True,skiprows=4,usecols=[0])
+p_orb = str(params[0])
+t_0 = str(params[1] - BJDREF)
+tdur = str(params[2])
+p_rot = str(params[3])
+Rp_Rs = str(params[4]**2.)
+impact = str(params[5])
+incl_orb = str(params[6])
+Teff = str(params[7])
+sden = str(params[8])
+
+
+# where is the original data file to analyze?
+#-- for dealing w/ Joe's model data:
+datafile = 'llama_lightcurve_raw.txt'
+tbad,ff0 = np.loadtxt(dropboxdir + datafile, unpack=True, usecols=(0,1), dtype=('string','float'))
+escale = 0.0002 # value i arrived at "a posteriori"
+ee0 = np.random.normal(loc=0,scale=escale,size=np.size(ff0))
+# remake time array
+dt = 5. / 60. / 24. # 5 min cadence
+tt0 = np.arange(np.size(ff0))*dt
+
+
+toff = float(tdur) / float(p_orb) # the transit duration in % (phase) units
+
+# down-sample the out-of-transit data
+# define the region +/- 1 transit duration for in-transit
+OOT = np.where( ((((tt0-float(t_0)) % float(p_orb))/float(p_orb)) > toff) &
+                ((((tt0-float(t_0)) % float(p_orb))/float(p_orb)) < 1-toff) )
+
+ITT = np.where( ((((tt0-float(t_0)) % float(p_orb))/float(p_orb)) <= toff) |
+                ((((tt0-float(t_0)) % float(p_orb))/float(p_orb)) >= 1-toff) )
+
+
+# the factor to down-sample by
+Nds = 30 # for Kepler 17
+# Nds = 10 # for Joe's data
+
+# down sample out-of-transit, grab every N'th data point
+OOT_ds = OOT[0][np.arange(0,np.size(OOT), Nds)]
+
+# use in- and out-of-transit data
+idx = np.concatenate((ITT[0][0:], OOT_ds))
+idx.sort()
+
+# these arrays are the final, down-sampled data
+tt = tt0[idx]
+# ff = ff0[idx]
+# ee = ee0[idx]
+#-- for Joe's model, add noise
+ff = np.add( np.array(ff0[idx], dtype='float'), ee0[idx] )
+ffraw = np.array(ff0[idx], dtype='float')
+ee = np.ones_like(ff) * escale
+
+
+plt.figure(figsize=(13,6))
+plt.scatter(tt, ffraw, marker='o')
+plt.plot(tt, ffraw)
+plt.xlim((855,915))
+plt.ylim((0.97, 1.0))
+plt.xlabel('Time (days)')
+plt.ylabel('Relative Flux')
+plt.savefig('/Users/james/Dropbox/research_projects/gj1243_spots/joe_lc_wide1.png', dpi=250)
+plt.show()
+
+
+plt.figure(figsize=(13,6))
+plt.scatter(tt, ffraw, marker='o')
+plt.plot(tt, ffraw)
+plt.xlim((143.1,144.1))
+plt.ylim((0.97,0.985))
+plt.xlabel('Time (days)')
+plt.ylabel('Relative Flux')
+plt.savefig('/Users/james/Dropbox/research_projects/gj1243_spots/joe_lc_trans.png', dpi=250)
+plt.show()
--- per_trans.py
+++ per_trans.py
@@ -17,138 +17,161 @@ from matplotlib import cm
 #--------- START OF SETUP ------------
 # CONFIG THESE THINGS FOR EACH RUN
 #-- which directory to run in? what settings?
-fname = 'k17'
-workingdir = '/astro/store/scratch/jrad/stsp/n8s/' # kepler17
-dropboxdir = '/astro/users/jrad/Dropbox/python/kepler17/' # kepler17
-BJDREF = 2454833. # kepler17
+# fname = 'k17'
+# workingdir = '/astro/store/scratch/jrad/stsp/n8s/' # kepler17
+# dropboxdir = '/astro/users/jrad/Dropbox/python/kepler17/' # kepler17
+# BJDREF = 2454833. # kepler17
 
 # fname = 'joe'
 # workingdir = '/astro/store/scratch/jrad/stsp/joe/' # joe model
 # dropboxdir = '/astro/users/jrad/Dropbox/python/kepler17/llama_model/' # joe model
 # BJDREF = 0.  # Joe model
 
+
+# workingdir = '/astro/store/scratch/jrad/stsp/kep17flat/' # kepler17
+
+
+# NOTE: must have -L mode run!
+
 #--------- END OF SETUP ------------
 
+def best_spots(dropboxdir = '/astro/users/jrad/Dropbox/python/kepler17/',
+               workingdir = '/astro/store/scratch/jrad/stsp/n8s/',
+               pfile='kep17.params', # fname='k17',
+               BJDREF = 2454833.):
+    '''
 
-#-- read in original .params file, get (t_0, p_orb) to find each transit
-pfile = dropboxdir + 'kep17.params'
+    :param dropboxdir:
+    :param workingdir:
+    :param fname:
+    :param pfile:
+    :param BJDREF:
+    :return:
+    '''
 
-params = np.loadtxt(pfile,unpack=True,skiprows=4,usecols=[0])
-p_orb = str(params[0])
-t_0 = str(params[1] - BJDREF)
-tdur = str(params[2])
-p_rot = str(params[3])
-Rp_Rs = str(params[4]**2.)
-# impact = str(params[5])
-# incl_orb = str(params[6])
-# Teff = str(params[7])
-# sden = str(params[8])
+    #-- read in original .params file, get (t_0, p_orb) to find each transit
+    # pfile = 'kep17.params'
+
+    params = np.loadtxt(dropboxdir + pfile, unpack=True, skiprows=4,usecols=[0])
+    p_orb = str(params[0])
+    t_0 = str(params[1] - BJDREF)
+    tdur = str(params[2])
+    # p_rot = str(params[3])
+    # Rp_Rs = str(params[4]**2.)
+    # impact = str(params[5])
+    # incl_orb = str(params[6])
+    # Teff = str(params[7])
+    # sden = str(params[8])
 
 
-# figure out number of spots by reading in first parambest file
-os.system('ls ' + workingdir + '*parambest*.txt > parambest.lis')
-pbestfile = np.loadtxt('parambest.lis', dtype='string')
+    # figure out number of spots by reading in first parambest file
+    os.system('ls ' + workingdir + '*parambest*.txt > parambest.lis')
+    pbestfile = np.loadtxt('parambest.lis', dtype='string')
 
-# read first file in to learn size
-tx = np.loadtxt(pbestfile[0], dtype='float', usecols=(0,), unpack=True, delimiter=' ')
-nspt = (len(tx) - 3.) / 2. / 3.
+    # read first file in to learn size
+    tx = np.loadtxt(pbestfile[0], dtype='float', usecols=(0,), unpack=True, delimiter=' ')
+    nspt = (len(tx) - 3.) / 2. / 3.
 
-chisq_tr = np.array([], dtype='float') # use np.append to add on to
-tmid_tr = np.array([], dtype='float')
+    chisq_tr = np.array([], dtype='float') # use np.append to add on to
+    tmid_tr = np.array([], dtype='float')
 
-rad = np.empty(nspt, dtype='float') # use np.vstack (or other stack?) to add on to
-lat = np.empty(nspt, dtype='float')
-lon = np.empty(nspt, dtype='float')
+    rad = np.empty(nspt, dtype='float') # use np.vstack (or other stack?) to add on to
+    lat = np.empty(nspt, dtype='float')
+    lon = np.empty(nspt, dtype='float')
 
-for n in range(len(pbestfile)):
-    # read in each lcbest file
-    # NOTE: needs to have _L mode done, we need the vis flag!
-    tn,fn,en,mn,flg = np.loadtxt(pbestfile[n].replace('parambest', 'L_lcout'),
-                                 dtype='float', unpack=True, usecols=(0,1,2,3,4))
-    np_l = nspt * 3 + 1
-    i = range(int(nspt))
+    for n in range(len(pbestfile)):
+        # read in each lcbest file
+        # NOTE: needs to have _L mode done, we need the vis flag!
+        tn,fn,en,mn,flg = np.loadtxt(pbestfile[n].replace('parambest', 'L_lcout'),
+                                     dtype='float', unpack=True, usecols=(0,1,2,3,4))
+        np_l = nspt * 3 + 1
+        i = range(int(nspt))
 
-    # read in corresponding parambest file
-    t = np.loadtxt(pbestfile[n], dtype='float', usecols=(0,), unpack=True, delimiter=' ')
+        # read in corresponding parambest file
+        t = np.loadtxt(pbestfile[n], dtype='float', usecols=(0,), unpack=True, delimiter=' ')
 
-    # step thru every transit in this lcbest file
-    trans0 = int(np.round((tn[0] - float(t_0))/float(p_orb)))
-    trans1 = int(np.round((tn[-1] - float(t_0))/float(p_orb)))
-    for j in range(trans0, trans1):
+        # step thru every transit in this lcbest file
+        trans0 = int(np.round((tn[0] - float(t_0))/float(p_orb)))
+        trans1 = int(np.round((tn[-1] - float(t_0))/float(p_orb)))
+        for j in range(trans0, trans1):
+            trans_j = float(t_0) + float(p_orb)*j
+            intrans = np.where((tn >= trans_j - float(tdur)*0.5) &
+                               (tn <= trans_j + float(tdur)*0.5))
+
+            if len(intrans[0])>0:
+                # for each transit, compute local chisq
+                chisq_tr = np.append(chisq_tr, np.sum( ((mn[intrans] - fn[intrans]) / en[intrans])**2.0 ))
+
+                tmid_tr = np.append(tmid_tr, np.mean(tn[intrans]))
+
+                flg_tr = flg[intrans]
+                r1 = np.zeros(nspt)
+                x1 = np.zeros(nspt)
+                y1 = np.zeros(nspt)
+                in_trans = np.zeros(nspt)
+                for i in range(int(nspt)):
+                    r1[i] = t[np_l + 1 + i*3.] # radius
+                    x1[i] = t[np_l + 2 + i*3.] # lat
+                    y1[i] = t[np_l + 3 + i*3.] # lon
+
+                    k = np.mod(flg_tr, 2)
+                    in_trans[i] = (k == 1.).sum()
+                    flg_tr = (flg_tr - k)/2.0
+
+                bad = np.where((in_trans<1)) # find spots not in THIS transit
+                if len(bad[0])>0:
+                    r1[bad] = -99 # give them bad values
+                    x1[bad] = -99
+                    y1[bad] = -99
+
+                rad = np.vstack( (rad, r1) )
+                lat = np.vstack( (lat, x1) )
+                lon = np.vstack( (lon, y1) )
+
+    rad = rad[1:,:] # get rid of first empty row
+    lat = lat[1:,:]
+    lon = lon[1:,:]
+
+
+    tmid_out = np.array([], dtype='float')
+
+    rad_out = np.empty(nspt, dtype='float') # use np.vstack (or other stack?) to add on to
+    lat_out = np.empty(nspt, dtype='float')
+    lon_out = np.empty(nspt, dtype='float')
+
+    # once every lcbest file read, pass thru and compare windows in same transit
+    for j in range(int( (max(tmid_tr)-min(tmid_tr))/float(p_orb)+1 )):
+        # the transit mid time
         trans_j = float(t_0) + float(p_orb)*j
-        intrans = np.where((tn >= trans_j - float(tdur)*0.5) &
-                           (tn <= trans_j + float(tdur)*0.5))
+        # find all transits that have same mid time, within some tolerance (0.1 days)
+        xtr = np.where((np.abs(tmid_tr - trans_j)<0.1))
+        if len(xtr[0])>0:
+            best = np.argmin(chisq_tr[xtr])
 
-        if len(intrans[0])>0:
-            # for each transit, compute local chisq
-            chisq_tr = np.append(chisq_tr, np.sum( ((mn[intrans] - fn[intrans]) / en[intrans])**2.0 ))
+            tmid_out = np.append(tmid_out, trans_j)
+            rad_out = np.vstack( (rad_out, rad[xtr,:][:,best]) )
+            lat_out = np.vstack( (lat_out, lat[xtr,:][:,best]) )
+            lon_out = np.vstack( (lon_out, lon[xtr,:][:,best]) )
 
-            tmid_tr = np.append(tmid_tr, np.mean(tn[intrans]))
+    rad_out = rad_out[1:,:] # get rid of first empty row
+    lat_out = lat_out[1:,:]
+    lon_out = lon_out[1:,:]
 
-            flg_tr = flg[intrans]
-            r1 = np.zeros(nspt)
-            x1 = np.zeros(nspt)
-            y1 = np.zeros(nspt)
-            in_trans = np.zeros(nspt)
-            for i in range(int(nspt)):
-                r1[i] = t[np_l + 1 + i*3.] # radius
-                x1[i] = t[np_l + 2 + i*3.] # lat
-                y1[i] = t[np_l + 3 + i*3.] # lon
+    return tmid_out, rad_out, lat_out, lon_out
 
-                k = np.mod(flg_tr, 2)
-                in_trans[i] = (k == 1.).sum()
-                flg_tr = (flg_tr - k)/2.0
+# plt.figure(figsize=(12,6))
+# for k in range(int(nspt)):
+#     yes = np.where((rad_out[:,k] > 0)) # pick spots not = -99
+#     plt.scatter(tmid_out[yes], lon_out[yes,k], alpha=0.6,
+#                 cmap=cm.gnuplot2_r, c=(rad_out[yes,k]),
+#                 s=(rad_out[yes,k] / np.nanmax(rad_out)*20.)**2.)
+# plt.xlim((np.min(tmid_out), np.max(tmid_out)))
+# plt.ylim((0,360))
+# plt.xlabel('Time (BJD - 2454833 days)')
+# plt.ylabel('Longitude (deg)')
+# plt.title('Best Transit Spots')
+# cb = plt.colorbar()
+# cb.set_label('spot radius')
+# plt.savefig('/astro/users/jrad/Dropbox/research_projects/gj1243_spots/'+fname+'_best_transits.png', dpi=250)
+# plt.show()
 
-            bad = np.where((in_trans<1)) # find spots not in THIS transit
-            if len(bad[0])>0:
-                r1[bad] = -99 # give them bad values
-                x1[bad] = -99
-                y1[bad] = -99
-
-            rad = np.vstack( (rad, r1) )
-            lat = np.vstack( (lat, x1) )
-            lon = np.vstack( (lon, y1) )
-
-rad = rad[1:,:] # get rid of first empty row
-lat = lat[1:,:]
-lon = lon[1:,:]
-
-
-tmid_out = np.array([], dtype='float')
-
-rad_out = np.empty(nspt, dtype='float') # use np.vstack (or other stack?) to add on to
-lat_out = np.empty(nspt, dtype='float')
-lon_out = np.empty(nspt, dtype='float')
-
-# once every lcbest file read, pass thru and compare windows in same transit
-for j in range(int( (max(tmid_tr)-min(tmid_tr))/float(p_orb)+1 )):
-    # the transit mid time
-    trans_j = float(t_0) + float(p_orb)*j
-    # find all transits that have same mid time, within some tolerance (0.1 days)
-    xtr = np.where((np.abs(tmid_tr - trans_j)<0.1))
-    if len(xtr[0])>0:
-        best = np.argmin(chisq_tr[xtr])
-
-        tmid_out = np.append(tmid_out, trans_j)
-        rad_out = np.vstack( (rad_out, rad[xtr,:][:,best]) )
-        lat_out = np.vstack( (lat_out, lat[xtr,:][:,best]) )
-        lon_out = np.vstack( (lon_out, lon[xtr,:][:,best]) )
-
-rad_out = rad_out[1:,:] # get rid of first empty row
-lat_out = lat_out[1:,:]
-lon_out = lon_out[1:,:]
-
-plt.figure(figsize=(12,6))
-for k in range(int(nspt)):
-    yes = np.where((rad_out[:,k] > 0)) # pick spots not = -99
-    plt.scatter(tmid_out[yes], lon_out[yes,k], alpha=0.6,
-                cmap=cm.gnuplot2_r, c=(rad_out[yes,k]),
-                s=(rad_out[yes,k] / np.nanmax(rad_out)*20.)**2.)
-plt.xlim((np.min(tmid_out), np.max(tmid_out)))
-plt.ylim((0,360))
-plt.xlabel('Time (BJD - 2454833 days)')
-plt.ylabel('Longitude (deg)')
-plt.title('Best Transit Spots')
-cb = plt.colorbar()
-cb.set_label('spot radius')
-plt.show()
